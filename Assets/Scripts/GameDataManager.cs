5560f33018801aa2a6464eaa5e61b8cd65957ccd
KarenKrill <vampart1997@outlook.com>
2024-07-10 20:04:02 +0500

    Added game data manager

---
+++ Assets/Scripts/GameDataManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using Assets.Scripts.Model;
+
+namespace Assets.Scripts
+{
+    public class GameDataManager
+    {
+        private static readonly string AppDirectory = Application.dataPath; // Application.persistentDataPath
+        private static readonly string SavedGameDataFileName = Path.Combine(AppDirectory, "GameData.json");
+        private GameData _defaultGameData, _gameData;
+        private GameDataManager() { }
+        private static Lazy<GameDataManager> _instance = new(() => new());
+        public static GameDataManager Instance => _instance.Value;
+        public GameData GameData => _gameData;
+        public void Init(GameData defaultGameData)
+        {
+            _defaultGameData = defaultGameData;
+            ResetToDefaults();
+        }
+        public bool IsSavedDataExists => File.Exists(SavedGameDataFileName);
+        public void Save()
+        {
+            var serializedGameData = JsonUtility.ToJson(_gameData, true);
+            File.WriteAllText(SavedGameDataFileName, serializedGameData);
+        }
+        public void Load()
+        {
+            if (IsSavedDataExists)
+            {
+                var serializedGameData = File.ReadAllText(SavedGameDataFileName);
+                JsonUtility.FromJsonOverwrite(serializedGameData, _gameData);
+            }
+        }
+        public void ResetToDefaults()
+        {
+            Model.Player playerClone = null, enemyClone = null;
+            playerClone = UnityEngine.Object.Instantiate(_defaultGameData.Player);
+            playerClone.InventoryConfig = UnityEngine.Object.Instantiate(_defaultGameData.Player.InventoryConfig);
+            enemyClone = UnityEngine.Object.Instantiate(_defaultGameData.Enemy);
+            enemyClone.InventoryConfig = UnityEngine.Object.Instantiate(_defaultGameData.Enemy.InventoryConfig);
+            if(_gameData != null)
+            {
+                if (_gameData.Player != null)
+                {
+                    if (_gameData.Player.InventoryConfig != null)
+                    {
+                        UnityEngine.Object.Destroy(_gameData.Player.InventoryConfig);
+                    }
+                    UnityEngine.Object.Destroy(_gameData.Player);
+                }
+                if (_gameData.Enemy != null)
+                {
+                    if (_gameData.Enemy.InventoryConfig != null)
+                    {
+                        UnityEngine.Object.Destroy(_gameData.Enemy.InventoryConfig);
+                    }
+                    UnityEngine.Object.Destroy(_gameData.Enemy);
+                }
+            }
+            _gameData = new(_defaultGameData.State, _defaultGameData.RoundNumber, _defaultGameData.RoundsCount, playerClone, enemyClone);
+        }
+    }
+}
